1469521aaf766a371bffea8bd57a4dc153277aa5
Zach Cohen <zachtylercohen@gmail.com>
2020-03-21 14:58:55 -0700

    color palette functions

--- drawing/2/sketch.js
+++ drawing/2/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  // put setup code here
+  createCanvas(500, 500);
 }
-
 function draw() {
-  // put drawing code here
+  background(250, 0, 250);
+  rectMode(CENTER);
+  //draw 1st rectangle
+  noStroke();
+  fill(151, 249, 249);
+  rect(50, 50, 1000, 100, 4);
+  //draw 2nd
+  fill(164, 222, 249);
+  rect(50, 150, 1000, 100, 4);
+  //draw 3rd
+  fill(193, 224, 247);
+  rect(50, 250, 1000, 100, 4);
+  //draw 4th
+  fill(207, 186, 225);
+  rect(50, 350, 1000, 100, 4);
+  //draw 5th
+  fill(197, 159, 201);
+  rect(50, 450, 1000, 100, 4);
 }
